4255261f37f91be46ffacae96dd7c5767df5c721
ZhaslanR <zhaslan_12@bk.ru>
2017-12-20 15:48:36 +0600

    Добавьте файлы проекта.

---
+++ CarbageCollectorLesson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbageCollectorLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> list = new List<User>();
+
+            for(int i = 0;i< 100000000;i++)
+            {
+                list.Add(new User { Login = "admin" + i, Password = "" + i});
+                Console.WriteLine(GC.GetGeneration(new User { Login = "admin" + i, Password = "" + i }));
+            }
+
+            for (int i = 0; i < 100000000; i++)
+            {
+                list.RemoveAt(i);
+            }
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ CarbageCollectorLesson/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbageCollectorLesson
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        ~User() { }
+        public User()
+        {
+
+        }
+    }
+}
